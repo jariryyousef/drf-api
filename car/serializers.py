dd2af6aca914483035b35226888e4468c950c992
jariryyousef@gmail.com <jariryyousef@gmail.com>
2021-11-21 17:57:33 +0200

    add test

--- car/serializers.py
+++ car/serializers.py
@@ -5,4 +5,4 @@ from .models import Car
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ("id", "title", "descripton", "auther","year","created_at","updated_at")
+        fields = ("id", "title", "descripton", "auther","year")
